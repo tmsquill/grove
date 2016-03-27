996fa360d864ce722cb757150ca2c2e5a22f9d7b
Zivia <zivia@unm.edu>
2016-03-27 16:03:16 -0600

    Updated requirements.txt for thriftpy

--- examples/grammar_argos/thrift/test/test_thrift.py
+++ examples/grammar_argos/thrift/test/test_thrift.py
@@ -1,4 +1,5 @@
 import thriftpy
+
 foraging_thrift = thriftpy.load("../foraging.thrift", module_name="foraging_thrift")
 
 print foraging_thrift
@@ -6,5 +7,20 @@ print dir(foraging_thrift)
 print foraging_thrift.Root
 root = foraging_thrift.Root()
 print root
-root.init = foraging_thrift.StateInitialize
+root.init = foraging_thrift.StateInitialize()
 print root
+print dir(root.init)
+print root.init.thrift_spec
+print root.init.default_spec
+root.init.init_2 = foraging_thrift.ProbInitState.PROB_INIT_STATE_0
+print root.init.thrift_spec
+print root.init.default_spec
+print root.init.init_2
+
+import thriftpy.transport as tran
+import thriftpy.protocol as prot
+
+transOut = tran.TMemoryBuffer()
+protocolOut = prot.TBinaryProtocol(transOut)
+root.write(protocolOut)
+print transOut.getvalue()
