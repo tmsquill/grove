491bc40ef3f61ca613af0a12f8aa51df8517b4dc
Zivia <zivia@unm.edu>
2016-04-25 09:51:26 -0600

    Added Simulation Test

---
+++ simulation/test/test_simulation.py
@@ -0,0 +1,18 @@
+import simulation.environment as environment
+import simulation.entity as entity
+import simulation.simulation as simulation
+import random
+
+# Create the entities for the simulation.
+agents = [entity.Agent(position=(random.randint(0, 20), random.randint(0, 20))) for _ in xrange(5)]
+nest = entity.Nest(position=(8, 8), size=(4, 4))
+food = [entity.Food(position=(random.randint(0, 20), random.randint(0, 20))) for _ in xrange(10)]
+
+entities = agents + [nest] + food
+
+# Create the environment for the simulation.
+env = environment.Environment()
+
+# Create and execute the simulation.
+sim = simulation.Simulation(environment=env, entities=entities)
+sim.execute()
